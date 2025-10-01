0b9c6ed8a861751351b92e6256bd86e3fd56d803
SpruebaC <sprueba@afundacion.org>
2025-10-01 13:36:47 +0200

    Ejercicio 1 Php

---
+++ apache1/www/site3/saludo.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<head>
+    <meta charset="UTF-8">
+    <title>Saludo</title>
+</head>
+<body>
+    <?php
+        echo "Hola, soy Santiago Cambeiro y hoy es " . date("l");
+    ?>
+</body>
+</html>
