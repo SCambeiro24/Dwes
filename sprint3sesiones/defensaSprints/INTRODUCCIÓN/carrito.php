a16a1444be979e8df112e9841c148d99846a808b
SpruebaC <sprueba@afundacion.org>
2025-11-05 13:49:06 +0100

    carrito

--- sprint3sesiones/defensaSprints/INTRODUCCIÓN/carrito.php
+++ sprint3sesiones/defensaSprints/INTRODUCCIÓN/carrito.php
@@ -3,29 +3,24 @@
     <h1>Carrito</h1>
     <?php
     /*Haz que se muestren los dos arrays sin usar arrays asociativos*/
-      $productos = array(
-        "Manzana",
-        "Pan",
-        "Lambo" 
-      );
-      $precios = array(
-        0.5,
-        1.2,
-        1000000.0
-      );
+      $productos = array("Manzana", "Pan", "Lambo");
+      $precios = array(0.5, 1.2, 1000000.0);
 
       $total = 0.0;
 
       echo "<table border='1'>";
       echo "<tr><th>Producto</th><th>Precio (€)</th></tr>";
 
-      foreach ($productos as $producto) {
-        
+	for ($i = 0; $i < count($productos); $i++) {
+        $producto = $productos[$i];
+        $precio = $precios[$i];  
+      
         echo "<tr>";
         echo "<td>" . $producto . "</td>";
-        echo "<td>" .  . "</td>";
+        echo "<td>" . $precio  . "</td>";
         echo "</tr>";
-        $total = $total + $precio;
+
+        $total += $precio;
       }
 
       echo "<tr><td><b>TOTAL</b></td><td><b>" . $total . "</b></td></tr>";
