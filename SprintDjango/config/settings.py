1cb84f2aa58bb6c57a54ab7f93433031d2f4da33
Santi <scambeiror24@fpcoruna.afundacion.org>
2026-01-12 09:39:31 +0100

    Django sprint

--- SprintDjango/config/settings.py
+++ SprintDjango/config/settings.py
@@ -33,7 +33,7 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     'django.contrib.admin',
     'rest_framework',
-    'peliculas',
+    'pelicula',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
