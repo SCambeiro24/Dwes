fc8dc72ea3837f08f39d87346365c17db1db2e15
Santi <scambeiror24@fpcoruna.afundacion.org>
2026-01-27 12:58:42 +0100

    Bloque 4: Configurar ViewSets y Routers para todos los recursos del sistema

--- SprintDjango/peliculas/urls.py
+++ SprintDjango/peliculas/urls.py
@@ -1,13 +1,15 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import PeliculaViewSet, GeneroViewSet
+from .views import (PeliculaViewSet, GeneroViewSet, ReseñaViewSet, UserViewSet, PerfilViewSet)
 
-# Definimos el router y registramos los ViewSets
+# Creamos el router y registramos todos los recursos del Diagrama ER
 router = DefaultRouter()
 router.register(r'peliculas', PeliculaViewSet, basename='pelicula')
 router.register(r'generos', GeneroViewSet, basename='genero')
+router.register(r'resenas', ReseñaViewSet, basename='resena')
+router.register(r'usuarios', UserViewSet, basename='usuario')
+router.register(r'perfiles', PerfilViewSet, basename='perfil')
 
-# Las URLs finales se generan automáticamente
 urlpatterns = [
     path('', include(router.urls)),
 ]
--- SprintDjango/peliculas/views.py
+++ SprintDjango/peliculas/views.py
@@ -1,8 +1,10 @@
+from django.contrib.auth.models import User
 from rest_framework.viewsets import ModelViewSet
-from .models import Pelicula, Genero
-from .serializers import PeliculaSerializer, GeneroSerializer
+from .models import Pelicula, Genero, Reseña, Perfil
+from .serializers import ( PeliculaSerializer, GeneroSerializer, ReseñaSerializer,  PerfilSerializer, UserSerializer)
 
-# ModelViewSet incluye: list, retrieve, create, update y destroy
+# ModelViewSet incluye automáticamente: list, retrieve, create, update y destroy
+
 class PeliculaViewSet(ModelViewSet):
     queryset = Pelicula.objects.all()
     serializer_class = PeliculaSerializer
@@ -10,3 +12,15 @@ class PeliculaViewSet(ModelViewSet):
 class GeneroViewSet(ModelViewSet):
     queryset = Genero.objects.all()
     serializer_class = GeneroSerializer
+
+class ReseñaViewSet(ModelViewSet):
+    queryset = Reseña.objects.all()
+    serializer_class = ReseñaSerializer
+
+class UserViewSet(ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+class PerfilViewSet(ModelViewSet):
+    queryset = Perfil.objects.all()
+    serializer_class = PerfilSerializer
